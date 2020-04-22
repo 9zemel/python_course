ae6e6a02c3cd0cb536bda620ab002079e933319c
Alyona Polyakova <epolyakova@ozon.ru>
2020-04-22 19:03:10 +0300

    thirdLesson's programm

---
+++ LessonThird/russianRoulette.py
@@ -0,0 +1,18 @@
+import random
+
+amountBullets = input("Сколько зарядить? ")
+aob = int(amountBullets)
+baraban = [0,0,0,0,0,0]
+
+for i in range(aob):
+    baraban[i] = 1
+
+howMuch = input("Сколько раз крутить? ")
+hm = int(howMuch)
+for i in range(hm):
+    random.shuffle(baraban)
+    if baraban[0] == 1:
+        print("Выстрел!")
+    else:
+        print("щелк")
+
